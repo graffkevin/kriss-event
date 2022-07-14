1f3efce9af869fc25c21e3ba0d70f80d57660193
unknown <51543682+graffkevin@users.noreply.github.com>
2022-07-14 17:04:12 +0200

    Configure React app for deployment to GitHub Pages

---
+++ website-poc/src/components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navigation from './navigation/Navigation'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navigation />
+      <div>
+        <p>coucou</p>
+      </div>
+    </div>
+  );
+  
+}
+
+export default App;
